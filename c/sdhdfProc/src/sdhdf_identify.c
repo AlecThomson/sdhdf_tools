527fe0de28066549b8e95f2b032019daac5c8af3
hob044 <george.hobbs@csiro.au>
2021-07-31 12:42:48 +1000

    Added lots of terms to select on

--- c/sdhdfProc/src/sdhdf_identify.c
+++ c/sdhdfProc/src/sdhdf_identify.c
@@ -38,18 +38,28 @@
 typedef struct infoStruct {
   char fname[MAX_STRLEN];
   char source[MAX_STRLEN];
+  char projid[MAX_STRLEN];
+  char hdrVersion[MAX_STRLEN];
+  char observer[MAX_STRLEN];
+  char rcvr[MAX_STRLEN];
+  char telescope[MAX_STRLEN];
+  int npol;
+  char calMode[20];
+  int scheduleNumber;
   float ra;
   float dec;
   double mjd;
   int nband;
+  int ndump;
   int sb;
+  
 } infoStruct;
 
 double haversine(double centre_long,double centre_lat,double src_long,double src_lat);
 double dms_turn(char *line);
 double hms_turn(char *line);
-int turn_hms(double turn, char *hms);
-int turn_dms(double turn, char *dms);
+int    turn_hms(double turn, char *hms);
+int    turn_dms(double turn, char *dms);
 double turn_deg(double turn);
 
 
@@ -58,20 +68,30 @@ void help()
   printf("sdhdf_identify\n\n");
   printf("Command line options\n\n");
   printf("-allBand                        - look for matches amongst all sub-bands in the files (otherwise use band defined by -sb)\n");
+  printf("-cal <id>                       - selects based on the noise source - usually ON or OFF\n");
   printf("-coord <ra0> <dec0> <raddist>   - selects file within a particular angular radius of those coordinates (RA = HH:MM:SS.SSS, DEC = DD:MM:SS.SSS, raddist = cone radius in degrees\n");
   printf("-h                              - this help\n");
+  printf("-hdrversion <num>               - selects based on SDHDF header version\n");
   printf("-info                           - display more information output lines\n");
+  printf("-npol <n>                       - selects based on the number of polarisation channels (1,2 or 4)\n");
   printf("-o <outFile>                    - write the output to an output file\n");
+  printf("-observer <name>                - selects based on specified observer\n");
   printf("-pair                           - produce paired 'on' and 'off' output - need -on and -off set\n");
   printf("-partial                        - does not require an exact match when checking source names\n");
+  printf("-rcvr <recevier>                - selects based on receiver name\n");
   printf("-sb <subband>                   - select sub-band number \n");
+  printf("-sched <id>                     - selects based on the scheduling block identifier\n");
   printf("-src <name>                     - selects based on source name\n");
+  printf("-tel <telescope>                - selects based on telescope name\n");
   printf("-nband <num>                    - selects based on the number of sub-bands in the file\n");
+  printf("-ndump <num>                    - selects based on the number of spectral dumps in the file\n");
   printf("-on <source name>               - defines 'on' source to be the specified name\n");
   printf("-off <source name>              - defines 'off' source to be the specified name\n");
   printf("\n\n");
   printf("Example: sdhdf_identify -pair -on 0407-658 -off 0407-658_R *.hdf\n");
-
+  printf("Example: sdhdf_identify -src 0407-658 *.hdf\n");
+  printf("Example: sdhdf_identify -cal ON *.hdf\n");
+  exit(1);
 }
 
 int main(int argc,char *argv[])
@@ -101,6 +121,26 @@ int main(int argc,char *argv[])
   int pair=0;
   char outFile[MAX_STRLEN];
   int useNband=0,selNband;
+  int useProjID=0;
+  char projID[MAX_STRLEN];
+  int useHdrVersion=0;
+  char hdrVersion[MAX_STRLEN];
+  int useObserver=0;
+  char observer[MAX_STRLEN];
+  int useRcvr=0;
+  char rcvr[MAX_STRLEN];
+  int useTelescope=0;
+  char telescope[MAX_STRLEN];
+  int useCalMode=0;
+  char calMode[MAX_STRLEN];
+  int useNpol=0;
+  int select_npol;
+  int useSchedNumber=0;
+  int select_sched;
+  int useNdump=0;
+  int select_ndump;
+
+  
   int writeOut=0;
   FILE *fout;
   int disp;
@@ -162,6 +202,24 @@ int main(int argc,char *argv[])
 	strcpy(onSrc[nOnSrc++],argv[++i]);
       else if (strcmp(argv[i],"-off")==0)
 	strcpy(offSrc[nOffSrc++],argv[++i]);
+      else if (strcasecmp(argv[i],"-projid")==0)
+	{  useProjID=1; strcpy(projID,argv[++i]); }
+      else if (strcasecmp(argv[i],"-hdrversion")==0)
+	{  useHdrVersion=1; strcpy(hdrVersion,argv[++i]); }
+      else if (strcasecmp(argv[i],"-observer")==0)
+	{  useObserver=1; strcpy(observer,argv[++i]); }
+      else if (strcasecmp(argv[i],"-rcvr")==0)
+	{  useRcvr=1; strcpy(rcvr,argv[++i]); }
+      else if (strcasecmp(argv[i],"-tel")==0)
+	{  useTelescope=1; strcpy(telescope,argv[++i]); }
+      else if (strcasecmp(argv[i],"-cal")==0)
+	{  useCalMode=1; strcpy(calMode,argv[++i]); }
+      else if (strcasecmp(argv[i],"-npol")==0)
+	{  useNpol=1; sscanf(argv[++i],"%d",&select_npol);}
+      else if (strcasecmp(argv[i],"-sched")==0)
+	{  useSchedNumber=1; sscanf(argv[++i],"%d",&select_sched);}
+      else if (strcasecmp(argv[i],"-ndump")==0)
+	{  useNdump=1; sscanf(argv[++i],"%d",&select_ndump);}
       else
 	{
 	  strcpy(fname[nFiles++],argv[i]);
@@ -192,6 +250,15 @@ int main(int argc,char *argv[])
 	      info[nEntry].mjd = inFile->beam[ibeam].bandData[iband].astro_obsHeader[0].mjd;
 	      info[nEntry].nband = inFile->beam[ibeam].nBand;
 	      info[nEntry].sb = iband;
+	      strcpy(info[nEntry].projid,inFile->primary[0].pid);
+	      strcpy(info[nEntry].hdrVersion,inFile->primary[0].hdr_defn_version);
+	      strcpy(info[nEntry].observer,inFile->primary[0].observer);
+	      strcpy(info[nEntry].rcvr,inFile->primary[0].rcvr);
+	      strcpy(info[nEntry].calMode,inFile->primary[0].cal_mode);
+	      info[nEntry].scheduleNumber = inFile->primary[0].sched_block_id;
+	      info[nEntry].ndump = inFile->beam[ibeam].bandHeader[iband].ndump;
+	      info[nEntry].npol = inFile->beam[ibeam].bandHeader[iband].npol;
+	      
 	      nEntry++;
 	    }
 	  else
@@ -205,6 +272,14 @@ int main(int argc,char *argv[])
 		  info[nEntry].mjd = inFile->beam[ibeam].bandData[j].astro_obsHeader[0].mjd;
 		  info[nEntry].nband = inFile->beam[ibeam].nBand;
 		  info[nEntry].sb = j;
+		  strcpy(info[nEntry].projid,inFile->primary[0].pid);
+		  strcpy(info[nEntry].hdrVersion,inFile->primary[0].hdr_defn_version);
+		  strcpy(info[nEntry].observer,inFile->primary[0].observer);
+		  strcpy(info[nEntry].rcvr,inFile->primary[0].rcvr);
+		  strcpy(info[nEntry].calMode,inFile->primary[0].cal_mode);
+		  info[nEntry].scheduleNumber = inFile->primary[0].sched_block_id;
+		  info[nEntry].ndump = inFile->beam[ibeam].bandHeader[j].ndump;
+		  info[nEntry].npol = inFile->beam[ibeam].bandHeader[j].npol;
 		  nEntry++;
 		}
 	    }
@@ -305,13 +380,94 @@ int main(int argc,char *argv[])
 		  else printf("%s\n",info[i].fname);		    
 		}
 	    }
-
+	  // Check project ID
+	  if (useProjID==1)
+	    {
+	      if (strcmp(info[i].projid,projID)==0)
+		{
+		  if (infoDisp==1) printf("[PROJID MATCH] %s %s sub-band %d\n",info[i].fname,info[i].projid,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
+	  // Check HdrVersion
+	  if (useHdrVersion==1)
+	    {
+	      if (strcmp(info[i].hdrVersion,hdrVersion)==0)
+		{
+		  if (infoDisp==1) printf("[HDR_VERSION MATCH] %s %s sub-band %d\n",info[i].fname,info[i].hdrVersion,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
+	  // Check observer
+	  if (useObserver==1)
+	    {
+	      if (strcmp(info[i].observer,observer)==0)
+		{
+		  if (infoDisp==1) printf("[OBSERVER MATCH] %s %s sub-band %d\n",info[i].fname,info[i].observer,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
+	  // Check receiver
+	  if (useRcvr==1)
+	    {
+	      if (strcmp(info[i].rcvr,rcvr)==0)
+		{
+		  if (infoDisp==1) printf("[RECEIVER MATCH] %s %s sub-band %d\n",info[i].fname,info[i].rcvr,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
+	  // Check telescope
+	  if (useTelescope==1)
+	    {
+	      if (strcmp(info[i].telescope,telescope)==0)
+		{
+		  if (infoDisp==1) printf("[TELESCOPE MATCH] %s %s sub-band %d\n",info[i].fname,info[i].telescope,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
+	  // Check cal_mode
+	  if (useCalMode==1)
+	    {
+	      if (strcmp(info[i].calMode,calMode)==0)
+		{
+		  if (infoDisp==1) printf("[CAL MATCH] %s %s sub-band %d\n",info[i].fname,info[i].calMode,info[i].sb);
+		  else printf("%s\n",info[i].fname);		    
+		}
+	    }
 	  // Check nband
 	  if (useNband == 1)
 	    {
 	      if (info[i].nband == selNband)
 		{
 		  if (infoDisp==1) printf("[NBAND MATCH] %s sub-band %d\n",fname[i],info[i].sb);
+		  else
+		    printf("%s\n",fname[i]);
+		}
+	    }
+	  // Check npol
+	  if (useNpol == 1)
+	    {
+	      if (info[i].npol  == select_npol)
+		{
+		  if (infoDisp==1) printf("[NPOL MATCH] %s sub-band %d\n",fname[i],info[i].sb);
+		  else
+		    printf("%s\n",fname[i]);
+		}
+	    }
+	  if (useSchedNumber == 1)
+	    {
+	      if (info[i].scheduleNumber  == select_sched)
+		{
+		  if (infoDisp==1) printf("[NPOL MATCH] %s sub-band %d\n",fname[i],info[i].sb);
+		  else
+		    printf("%s\n",fname[i]);
+		}
+	    }
+	  if (useNdump == 1)
+	    {
+	      if (info[i].ndump  == select_ndump)
+		{
+		  if (infoDisp==1) printf("[NPOL MATCH] %s sub-band %d\n",fname[i],info[i].sb);
 		  else
 		    printf("%s\n",fname[i]);
 		}
