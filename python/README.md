1887001d07d68fe399ce7002411a231b43131570
Thomson, Alec (S&A, Kensington) <AlecThomson@users.noreply.github.com>
2023-01-24 13:50:31 +0800

    Docs

--- python/README.md
+++ python/README.md
@@ -1,3 +1,3 @@
-# pySDHDF
+## A Python interface for SDHDF data
 
 Python classes for reading, interactive with, and writing SDHDF files.
--- python/pySDHDF/sdhdf.py
+++ python/pySDHDF/sdhdf.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Core SDHDF module
+"""
 
 from dataclasses import dataclass
 from pathlib import Path
