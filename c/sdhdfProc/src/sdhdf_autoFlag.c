7e7d35ef74cc10b8760c890a710cd60fb0f37451
hob044 <george.hobbs@csiro.au>
2023-11-06 10:30:06 +1100

    Improved transient detection code

--- c/sdhdfProc/src/sdhdf_autoFlag.c
+++ c/sdhdfProc/src/sdhdf_autoFlag.c
@@ -43,6 +43,7 @@ void flagDataPoint(sdhdf_fileStruct *inFile,int b,int i,int j,int s);
 void flagPersistentRFI(sdhdf_fileStruct *inFile,sdhdf_rfi *rfi,int nRFI,int b,int i);
 void flagTransientRFI(sdhdf_fileStruct *inFile,sdhdf_transient_rfi *transient_rfi,int nTransientRFI,int b,int i);
 void flagAutoDump(sdhdf_fileStruct *inFile,float autoFlagDump_sigma,int b,int i);
+void flagNarrowRFI(sdhdf_fileStruct *inFile,int b,int i);
 
 void help()
 {
@@ -72,7 +73,8 @@ int main(int argc,char *argv[])
   int bandEdgeFlag = 0;
   int persistentFlag = 0;
   int transientFlag = 0;
-
+  int narrowFlag = 0;
+  
   double freq;
   int flagIt;
   int haveFlag;
@@ -104,6 +106,7 @@ int main(int argc,char *argv[])
       else if (strcmp(argv[i],"-edge")==0) {sscanf(argv[++i],"%f",&bandEdge); bandEdgeFlag=1;}
       else if (strcmp(argv[i],"-persistent")==0) {persistentFlag=1;}
       else if (strcmp(argv[i],"-transient")==0) {transientFlag=1;}
+      else if (strcmp(argv[i],"-narrow")==0) {narrowFlag=1;}
       else if (strcasecmp(argv[i],"-autoFlagDump")==0) {autoFlagDump=1; sscanf(argv[++i],"%f",&autoFlagDump_sigma);}
       else if (strcmp(argv[i],"-h")==0) help();
       else if (strcasecmp(argv[i],"-flag")==0)
@@ -137,7 +140,7 @@ int main(int argc,char *argv[])
       sdhdf_copyRemainder(inFile,outFile,0);      
       sdhdf_loadMetaData(outFile);
 
-      // Load the trnasient and persistent RFI
+      // Load the transient and persistent RFI
       if (transientFlag==1)
 	sdhdf_loadTransientRFI(transient_rfi,&nTransientRFI,MAX_RFI,inFile->primary[0].telescope);
       if (persistentFlag==1)
@@ -164,6 +167,8 @@ int main(int argc,char *argv[])
 		flagTransientRFI(inFile,transient_rfi,nTransientRFI,b,i);
 	      if (nManualRFI > 0)
 		flagPersistentRFI(inFile,manual_rfi,nManualRFI,b,i);
+	      if (narrowFlag == 1)
+		flagNarrowRFI(inFile,b,i);
 	      if (autoFlagDump == 1)
 		flagAutoDump(inFile,autoFlagDump_sigma,b,i);
 
@@ -300,8 +305,8 @@ void flagTransientRFI(sdhdf_fileStruct *inFile,sdhdf_transient_rfi *transient_rf
 	  || (transient_rfi[k].f1 > inFile->beam[b].bandHeader[i].f0 && transient_rfi[k].f1 < inFile->beam[b].bandHeader[i].f1))
 	{
 	  // Now zap from each sub-band
-	  printf("Checking transient RFI: %s comparing %g -> %g with %g -> %g\n",transient_rfi[k].description,
-		 transient_rfi[k].f0,transient_rfi[k].f1,transient_rfi[k].f2,transient_rfi[k].f3);
+	  printf("Checking transient RFI: %s comparing %g -> %g with %g -> %g, type = %d\n",transient_rfi[k].description,
+		 transient_rfi[k].f0,transient_rfi[k].f1,transient_rfi[k].f2,transient_rfi[k].f3,transient_rfi[k].type);
 	  for (s=0;s<inFile->beam[b].bandHeader[i].ndump;s++)
 	    {
 	      v1 = v2 = v3 = 0;
@@ -315,7 +320,8 @@ void flagTransientRFI(sdhdf_fileStruct *inFile,sdhdf_transient_rfi *transient_rf
 		    {
 		      sumPol = (inFile->beam[b].bandData[i].astro_data.pol1[j+s*inFile->beam[b].bandHeader[i].nchan] 
 				+ inFile->beam[b].bandData[i].astro_data.pol2[j+s*inFile->beam[b].bandHeader[i].nchan]);
-		      if (v1 < sumPol) v1 = sumPol;
+		      if (transient_rfi[k].type == 1 && v1 < sumPol) v1 = sumPol;
+		      else if (transient_rfi[k].type == 2) v1 += sumPol;
 		      n1 ++;
 		    }
 		  if (freq > transient_rfi[k].f2 && freq < transient_rfi[k].f3)
@@ -326,6 +332,7 @@ void flagTransientRFI(sdhdf_fileStruct *inFile,sdhdf_transient_rfi *transient_rf
 		      n2 ++;
 		    }
 		}
+	      if (transient_rfi[k].type == 2) v1/=(double)n1;
 	      v2/=(double)n2; // SHOULD CHECK n2 > 0 --- FIX ME
 	      
 	      
@@ -350,6 +357,74 @@ void flagTransientRFI(sdhdf_fileStruct *inFile,sdhdf_transient_rfi *transient_rf
 		    }					  
 		}
 	    }
+	}
+    }
+}
+
+void flagNarrowRFI(sdhdf_fileStruct *inFile,int b,int i)
+{
+  int j,k,s;
+  int j0,j1,j2,j3;
+  double v1,v2,v3;
+  double mean,sdev;
+  int n1,n2;
+  int flagIt=0;
+  double freq;
+  double sumPol=0;
+  int blSamples  = 4;
+  int rfiSamples = 1;
+  // ii, b, i (band)
+
+
+
+  printf("Flagging narrow spectral features\n");
+  
+
+  for (s=0;s<inFile->beam[b].bandHeader[i].ndump;s++)
+    {
+      
+      n1=n2=0;
+      flagIt=0;
+      for (j=0;j<inFile->beam[b].bandHeader[i].nchan;j++)
+	{
+	  sumPol = (inFile->beam[b].bandData[i].astro_data.pol1[j+s*inFile->beam[b].bandHeader[i].nchan] 
+		    + inFile->beam[b].bandData[i].astro_data.pol2[j+s*inFile->beam[b].bandHeader[i].nchan]);
+	  v1 = sumPol;
+	  j0 = j-blSamples;
+	  if (j0 < 0) j0 = 0;
+	  j1 = j-1;
+	  if (j1 < 0) j1 = 0;
+	  j2 = j+rfiSamples;
+	  j3 = j2+blSamples;
+	  if (j2 > inFile->beam[b].bandHeader[i].nchan) j2 = inFile->beam[b].bandHeader[i].nchan;
+	  if (j3 > inFile->beam[b].bandHeader[i].nchan) j3 = inFile->beam[b].bandHeader[i].nchan;
+	  
+	  // Need to account for flagged channels ** FIX ME
+	  n1 = n2 = 0;
+	  v2 = v3 = 0;
+
+	  for (k=j0;k<j1;k++)
+	    {
+	      sumPol = (inFile->beam[b].bandData[i].astro_data.pol1[k+s*inFile->beam[b].bandHeader[i].nchan] 
+			+ inFile->beam[b].bandData[i].astro_data.pol2[k+s*inFile->beam[b].bandHeader[i].nchan]);
+	      v2 += sumPol;
+	      v3 += pow(sumPol,2);
+	      n1++;
+	    }
+	  for (k=j2;k<j3;k++)
+	    {
+	      sumPol = (inFile->beam[b].bandData[i].astro_data.pol1[k+s*inFile->beam[b].bandHeader[i].nchan] 
+			+ inFile->beam[b].bandData[i].astro_data.pol2[k+s*inFile->beam[b].bandHeader[i].nchan]);
+	      v2 += sumPol;
+	      v3 += pow(sumPol,2);
+	      n1++;
+	    }
+	  mean = v2/(double)n1;
+	  sdev = sqrt(1./(double)n1*v3 - pow(1.0/(double)n1 * v2,2));
+	  //	  printf("CHECKING NARROW: %g %g %g %d (V2 = %g, %g)\n",v1,mean,sdev,n1,v2,v3);
+		
+	  if (v1 > mean + 5*sdev) // FIX SDEV
+	    flagDataPoint(inFile,b,i,j,s);	    
 	}
     }
 }
